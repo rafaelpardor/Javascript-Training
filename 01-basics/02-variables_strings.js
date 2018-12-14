749de3b153397712a8952cb52f69f254874318e7
rafaelpardor <rafaelpardo98@gmail.com>
2018-12-14 12:36:56 -0500

    clase-02 - Manipulando strings 2

--- 01-basics/02-variables_strings.js
+++ 01-basics/02-variables_strings.js
@@ -1,8 +1,20 @@
 var nombre = 'Rafael',
     apellido = 'Pardo'
+var nombreComleto = `${nombre} ${apellido}`
+var lengthNombre = nombre.length
+var primeraLetraDeNombre = nombre.charAt(0)
+var ultimaLetraDeNombre = nombre.charAt(nombre.length - 1)
 var nombreEnMayusculas = nombre.toUpperCase()
 var apellidoEnMinusculas = apellido.toLowerCase()
+var lengthNombreCompleto = nombreComleto.length
+var str = nombreComleto.substr(0, 6)
 
-
-console.log(nombre,apellido)
-console.log(nombreEnMayusculas,apellidoEnMinusculas)
+console.log(nombre)
+console.log(apellido)
+console.log(`Largo del nombre: ${lengthNombre}`)
+console.log(`Primera letra del nombre: ${primeraLetraDeNombre}`)
+console.log(`Ultima letra del nombre: ${ultimaLetraDeNombre}`)
+console.log(`Nombre y apellidos modificados: ${nombreEnMayusculas} ${apellidoEnMinusculas}`)
+console.log(`Nombre y apellido: ${nombreComleto}`)
+console.log(`Largo de nombre completo: ${lengthNombreCompleto}`)
+console.log(`String manipulado: ${str}`)
