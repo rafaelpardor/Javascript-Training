096020812484b6b2cc2e6e5d32a745c77b5e5b86
rafaelpardor <rafaelpardo98@gmail.com>
2018-12-13 22:32:31 -0500

    clase-02 -  Manipulando Strings

---
+++ 01-basics/02-variables_strings.js
@@ -0,0 +1,8 @@
+var nombre = 'Rafael',
+    apellido = 'Pardo'
+var nombreEnMayusculas = nombre.toUpperCase()
+var apellidoEnMinusculas = apellido.toLowerCase()
+
+
+console.log(nombre,apellido)
+console.log(nombreEnMayusculas,apellidoEnMinusculas)
