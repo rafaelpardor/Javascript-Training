b57eaa01dbf5b65038ed3a63c4cc189725f75be1
rafaelpardor <rafaelpardo98@gmail.com>
2018-12-15 18:18:56 -0500

    clase-09 - Comparando objetos 2

--- 01-basics/06-comparaciones.js
+++ 01-basics/06-comparaciones.js
@@ -6,5 +6,8 @@ var otroRafael = {
     nombre: 'Rafael'
 }
 
+var otroRafa = rafael
+
 console.log(rafael == otroRafael)
 console.log(rafael.nombre == otroRafael.nombre)
+console.log(otroRafa == rafael)
