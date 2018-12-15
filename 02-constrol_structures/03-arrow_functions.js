992c610c0e600f3c82939e560d457e5778e7d57c
rafaelpardor <rafaelpardo98@gmail.com>
2018-12-15 18:47:20 -0500

    clase-12 - Arrow functions

---
+++ 02-constrol_structures/03-arrow_functions.js
@@ -0,0 +1,21 @@
+var rafael = {
+    nombre: 'Rafael',
+    apellido: 'Pardo Rodriguez',
+    edad: 20,
+}
+
+const MAYORIA_DE_EDAD = 18
+
+var esMayorDeEdad = (persona) => {
+    return persona.edad >= MAYORIA_DE_EDAD
+}
+
+function imprimirSiEsMayorDeEdad(persona) {
+    if (esMayorDeEdad(persona)) {
+        console.log(`${persona.nombre} es mayor de edad.`)
+    } else {
+        console.log(`${persona.nombre} aún no es mayor de edad.`)
+    }
+}
+
+imprimirSiEsMayorDeEdad(rafael)
