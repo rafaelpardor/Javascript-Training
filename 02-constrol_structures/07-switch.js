95b8005917c29cd4d21dfcef6cbdf03c8b2f3994
rafaelpardor <rafaelpardo98@gmail.com>
2018-12-15 21:45:19 -0500

    clase-16 - Switch statement

---
+++ 02-constrol_structures/07-switch.js
@@ -0,0 +1,42 @@
+var signo = prompt('¿Cual es tu signo?')
+
+switch (signo) {
+    case 'aries':
+        console.log('Usted es aries.')
+        break;
+    case 'tauro':
+        console.log('Usted es tauro.')
+        break;
+    case 'geminis':
+        console.log('Usted es geminis')
+        break;
+    case 'cancer':
+        console.log('Usted es cancer')
+        break;
+    case 'leo':
+        console.log('Usted es leo')
+        break;
+    case 'virgo':
+        console.log('Usted es virgo')
+        break;
+    case 'libra':
+        console.log('Usted es libra')
+        break;
+    case 'escorpion', 'escorpión':
+        console.log('Usted es escorpion')
+        break;
+    case 'sagitario':
+        console.log('Usted es sagitario')
+        break;
+    case 'capricornio':
+        console.log('Usted es capricornio')
+        break;
+    case 'acuario':
+        console.log('Usted es acuario')
+        break;
+    case 'piscis':
+        console.log('Usted es piscis.')
+        break;
+    default:
+        console.log('You\'re gay')
+}
