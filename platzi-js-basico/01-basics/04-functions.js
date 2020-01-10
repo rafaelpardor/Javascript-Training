1875ef7f5864b3c81c4b2f7a4e5291199f78f608
rafaelpardor <rafaelpardor@protonmail.com>
2020-01-10 14:03:29 -0500

    Functios

--- platzi-js-basico/01-basics/04-functions.js
+++ platzi-js-basico/01-basics/04-functions.js
@@ -1,11 +1,24 @@
-var nombre = 'Rafael';
-console.log(nombre);
+var name = 'Rafael', age = 21;
+console.log(name, age);
+
+// Funcion basica
+function suma(number) {
+	return number + number
+}
+console.log("Suma:",suma(10))
+
+function greeting(name, age) {
+	console.log(`${name} tiene ${age} anios`);
+}
+greeting(name, age);
+greeting('Linux', 45);
 
 // Funcion con alcanze local
-function imprimirNombreEnMayusculas(nombre) {
-    nombre = nombre.toUpperCase();
-    console.log("Variable modificada con la funcion",nombre);
+function imprimirNombreEnMayusculas(name) {
+    upperName = name.toUpperCase();
+    console.log("Variable modificada con la funcion:", upperName);
 }
 
-imprimirNombreEnMayusculas(nombre);
-console.log("Variable global no modificada:",nombre);
+imprimirNombreEnMayusculas('lucian');
+imprimirNombreEnMayusculas(name);
+console.log("Variable global no modificada:", name);
