00a585be7e89a50bfc53acde5cc9217112c40ff7
rafaelpardor <rafaelpardor@protonmail.com>
2020-01-11 17:04:41 -0500

    OOP js

--- platzi-js-basico/04.oop/01-oop.js
+++ platzi-js-basico/04.oop/01-oop.js
@@ -1,40 +1,41 @@
 // Clase para crear una persona
-class Persona {
-  constructor(nombre, apellido, edad, altura) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.edad = edad;
-    this.altura = altura;
+class Person {
+  constructor(firstName, lastName, age, height) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.height = height;
   }
-  saludar() {
+
+  // Method
+  greet() {
     console.log(
-      `Hola, me  llamo ${this.nombre} ${this.apellido} y tengo ${
-        this.edad
-      } años.`
+      `Hola, me  llamo ${this.firstName} ${this.lastName} y tengo ${this.age} años.`
     );
   }
-  saberAltura() {
-    return this.altura >= 1.8;
+
+  knowHeight() {
+    return this.height >= 1.8;
   }
 }
 
-class Desarrollador extends Persona {
-  constructor(nombre, apellido, edad, altura) {
-    super(nombre, apellido, edad, altura);
+class Developer extends Person {
+  constructor(firstName, lastName, age, height) {
+    super(firstName, lastName, age, height);
   }
-  saludar() {
+  greet() {
     console.log(
-      `Hola, soy ${this.nombre} ${this.apellido} y soy desarrollador.`
+      `Hola, soy ${this.firstName} ${this.lastName} y soy desarrollador.`
     );
   }
 }
 
 // Se crea un nuevo objeto
-var rafael = new Persona("Rafael", "Pardo R.", 20, 1.8);
-var juan = new Desarrollador("Juan", "Pardo R.", 24, 1.84);
+var rafael = new Person("Rafael", "Pardo R.", 20, 1.75);
+var juan = new Developer("Juan", "Pardo R.", 24, 1.84);
 console.log(rafael);
 console.log(juan);
 
-rafael.saludar();
-console.log(rafael.saberAltura());
-juan.saludar();
+rafael.greet();
+console.log(rafael.knowHeight());
+juan.greet();
