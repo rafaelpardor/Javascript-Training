cb9d67a49c26c1324041fdd1cc92b2fda27d0a40
rafaelpardor <rafaelpardor@protonmail.com>
2020-01-18 22:59:11 -0500

    Promises

--- platzi-js/jqueryJS/src/js/home.js
+++ platzi-js/jqueryJS/src/js/home.js
@@ -6,3 +6,20 @@ let change = "@rafaelpardor"
 function changeName(newName) {
   change = newName
 }
+
+const getUser = new Promise(function(response, err) {
+    // CallAPI
+    setTimeout(function() {
+        // 3 seconds
+        err();
+    }, 3000)
+})
+
+getUser
+    .then(function(){
+        console.log('All ok');
+    })
+    .catch(function(){
+        console.log("Error")
+    })
+
